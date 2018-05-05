f457561dd16db704119163077b8c294ff703ddd1
SatoShun <shun.sato1@gmail.com>
2018-05-05 07:27:28 +0000

    add javadoc

--- rxkeeporder-kotlin/src/main/java/com/github/satoshun/io/reactivex/keeporder/RxKeepOrder.kt
+++ rxkeeporder-kotlin/src/main/java/com/github/satoshun/io/reactivex/keeporder/RxKeepOrder.kt
@@ -8,8 +8,27 @@ import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
 
+/**
+ * attach [Flowable] stream and [RxKeepOrder].
+ */
 inline fun <T> Flowable<T>.keepOrder(seed: RxKeepOrder): Flowable<T> = this.compose(seed.attach())
+
+/**
+ * attach [Observable] stream and [RxKeepOrder].
+ */
 inline fun <T> Observable<T>.keepOrder(seed: RxKeepOrder): Observable<T> = this.compose(seed.attach())
+
+/**
+ * attach [Single] stream and [RxKeepOrder].
+ */
 inline fun <T> Single<T>.keepOrder(seed: RxKeepOrder): Single<T> = this.compose(seed.attach())
+
+/**
+ * attach [Maybe] stream and [RxKeepOrder].
+ */
 inline fun <T> Maybe<T>.keepOrder(seed: RxKeepOrder): Maybe<T> = this.compose(seed.attach())
+
+/**
+ * attach [Completable] stream and [RxKeepOrder].
+ */
 inline fun Completable.keepOrder(seed: RxKeepOrder): Completable = this.compose(seed.attach<Any>())
