ef99c3931f75ee9e5b371e3fc3d9a5b23619893f
SatoShun <shun.sato1@gmail.com>
2018-05-05 07:24:34 +0000

    add inline modifier

--- rxkeeporder-kotlin/src/main/java/com/github/satoshun/io/reactivex/keeporder/RxKeepOrder.kt
+++ rxkeeporder-kotlin/src/main/java/com/github/satoshun/io/reactivex/keeporder/RxKeepOrder.kt
@@ -1,3 +1,5 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package com.github.satoshun.io.reactivex.keeporder
 
 import io.reactivex.Completable
@@ -6,8 +8,8 @@ import io.reactivex.Maybe
 import io.reactivex.Observable
 import io.reactivex.Single
 
-fun <T> Flowable<T>.keepOrder(seed: RxKeepOrder): Flowable<T> = this.compose(seed.attach())
-fun <T> Observable<T>.keepOrder(seed: RxKeepOrder): Observable<T> = this.compose(seed.attach())
-fun <T> Single<T>.keepOrder(seed: RxKeepOrder): Single<T> = this.compose(seed.attach())
-fun <T> Maybe<T>.keepOrder(seed: RxKeepOrder): Maybe<T> = this.compose(seed.attach())
-fun Completable.keepOrder(seed: RxKeepOrder): Completable = this.compose(seed.attach<Any>())
+inline fun <T> Flowable<T>.keepOrder(seed: RxKeepOrder): Flowable<T> = this.compose(seed.attach())
+inline fun <T> Observable<T>.keepOrder(seed: RxKeepOrder): Observable<T> = this.compose(seed.attach())
+inline fun <T> Single<T>.keepOrder(seed: RxKeepOrder): Single<T> = this.compose(seed.attach())
+inline fun <T> Maybe<T>.keepOrder(seed: RxKeepOrder): Maybe<T> = this.compose(seed.attach())
+inline fun Completable.keepOrder(seed: RxKeepOrder): Completable = this.compose(seed.attach<Any>())
